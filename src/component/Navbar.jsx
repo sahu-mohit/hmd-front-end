5bed045fcb225e885a1781aed405dd0268665c0c
Anmol Rastogi <rastogiguru123@gmail.com>
2024-03-15 00:48:50 +0530

    name changes

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -59,7 +59,7 @@ export default function Navbar() {
           </div>
         </div>
         <div className="navbar-center">
-          <a className="btn btn-ghost text-xl underline">ANMOL BRANCH</a>
+          <a className="btn btn-ghost text-xl underline">ANMOL Rastogi</a>
         </div>
         <div className="flex-none gap-2 navbar-end">
           <label className="swap swap-rotate">
