a3ee10e3d0fa6ecc56a2aa8bfc800e438f14d489
Mohit Sahu <mohit.sahu2017@gmail.com>
2024-03-14 20:55:58 +0530

    MAIN BRANCH

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -53,7 +53,7 @@ export default function Navbar() {
                 <a>Contact</a>
               </li>
               <li>
-                <a>About</a>
+                <a>About MAIN BRANCH</a>
               </li>
             </ul>
           </div>
